316391d013e919e87bb3976b8ee34c99f6598098
yes damy <hamzty@gmail.com>
2022-05-21 10:32:59 +0300

    first chart added

--- mechanics.js
+++ mechanics.js
@@ -1,4 +1,7 @@
-const labels = [
+const days = []
+
+
+const hours = [
      '1 AM',
      '2 AM',
      '3 AM',
@@ -24,31 +27,116 @@ const labels = [
      '11 PM',
      '12 PM',
 ];
- 
-const data = {
-     labels: labels,
-     datasets: [{
-       label: 'My First dataset',
-       backgroundColor: '#5F6673',
-       borderColor: '#8C7161',
-       data: [0, 10, 5, 2, 20, 30, 45],
-     }]
-   };
- 
-const config = {
+
+const textuals = {
+     en: {
+          navbarLabels:{
+               philosophy: 'PHILOSOPHY',
+               about: 'ABOUT',
+               en: 'en',
+               ar: 'ar',
+               light: 'light',
+               dark: 'dark'
+          },
+          frstCard: {
+               K: '24K',
+               measureUnit: '-gold gram price',
+               currency: 'sar'
+          },
+          scndCard: {
+               K: '24K',
+               title1: '-gold',
+               measureUnit: ['1 g', '5 g', '10 g', '20 g', '1 oz', '50 g', '100 g', '200 g', '500 g', '1 kg'],
+               title2: 'price',
+               currencyList: ['usd', 'sar']
+          },
+          thrdCard: {
+               title: 'gold price over 24 hours'
+          }
+     },
+     ar: {
+
+     }
+}
+
+// the api call for the current price of gold
+
+
+// set values based on the page width for responsivity purposes
+Chart.defaults.font.size = 12
+
+let pageWidth = document.body.offsetWidth;
+if (pageWidth < 600){
+     Chart.defaults.font.size = 15
+}
+else if ( pageWidth < 1200){
+     Chart.defaults.font.size = 18
+}
+else if ( pageWidth < 1500){
+     Chart.defaults.font.size = 21
+}
+else {
+     Chart.defaults.font.size = 24
+}
+
+
+
+// the general font of all the charts
+Chart.defaults.font.family = "'Inknut Antiqua', 'Tomorrow'"
+
+// the function that controls the charts where the data are undefined 
+const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
+const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+
+// selects the canves where to imped the first chart
+const monthChart = document.getElementById('hoursChart').getContext('2d');
+
+
+const chart1 = new Chart(monthChart, {
+     // specifies the type of the chart(whether a bar, pie, table or a line chart)
      type: 'line',
-     data: data,
+     
+     // here we place our data 
+     data: {
+          labels: hours,
+          datasets: [{
+               label: textuals.en.thrdCard.title,
+               backgroundColor: '#8C7161aa',
+               borderColor: '#8C7161',
+               color: '#0d0d0d',
+               data: [222.64, 222.3, 219.2, 219.19, 220.47, 227.24, 227, 227, 218, NaN, NaN, 234, 222],
+
+               pointRadius: 3.4,
+
+               segment: {
+                    borderColor: ctx => skipped(ctx, '#8C716188') || down(ctx, '#8C7161'),
+                    borderDash: ctx => skipped(ctx, [6, 6]),
+                  },
+               spanGaps: true,
+
+               tension: 0,
+          }]
+     },
+     // here we place the some of the styling propereties
      options: {
           maintainAspectRatio: false,
           scales: {
                y: {
-                    beginAtZero: true
+                    suggestedMin: 220,
+                    suggestedMax: 230
+               },
+               x: {
+                    beginAtZero: true,
+                    suggestedMax: 2
                }
+          },
+          layout: {
+
+          },
+          responsive: true,
+          plugins: {
+               suggestedMin: 10,
+               suggestedMax: 10
           }
      }
-   };
-
-const myChart = new Chart(
-     document.getElementById('myChart'),
-     config
-   );  
+})
