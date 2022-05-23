a7c2b150d0122dec8d66e4bd5a543f661d1adeb2
yes damy <hamzty@gmail.com>
2022-05-23 16:41:05 +0300

    second chart initialized

--- mechanics.js
+++ mechanics.js
@@ -1,4 +1,35 @@
-const days = []
+const days = [
+     '1 May',
+     '2 May',
+     '3 May',
+     '4 May',
+     '5 May',
+     '6 May',
+     '7 May',
+     '8 May',
+     '9 May',
+     '10 May',
+     '11 May',
+     '12 May',
+     '13 May',
+     '14 May',
+     '15 May',
+     '16 May',
+     '17 May',
+     '18 May',
+     '19 May',
+     '20 May',
+     '21 May',
+     '22 May',
+     '23 May',
+     '24 May',
+     '25 May',
+     '26 May',
+     '27 May',
+     '28 May',
+     '29 May',
+     '30 May',
+]
 
 
 const hours = [
@@ -170,6 +201,7 @@ let localTimeNow = new Date();
 const hourMS = 60000
 const dayMS = 3600000
 
+// detect if its the daylight saving time in the united states or not, so the formual to calculate the gap between utc and est timezones
 let estOffset = -5
 if (localTimeNow.getMonth() > 3 && localTimeNow.getMonth() < 11){
      estOffset = -4
@@ -177,8 +209,10 @@ if (localTimeNow.getMonth() > 3 && localTimeNow.getMonth() < 11){
 
 let utc = localTimeNow.getTime() + (localTimeNow.getTimezoneOffset() * hourMS);
 
+// turns a timestamp to human readable date and time
 let est = new Date(utc + (dayMS*estOffset))
 
+// check if it the time when the gold markets are closed then it changes the fourth card contant based on this decision
 if ((est.getDay() == 5 && est.getHours() > 17) || est.getDay == 6 || (est.getDay() == 7 && est.getHours() < 6)) {
      marketState.innerHTML = 'Markets are closed now'
      marketState.style.color = offColor
@@ -192,7 +226,7 @@ if ((est.getDay() == 5 && est.getHours() > 17) || est.getDay == 6 || (est.getDay
 // set values based on the page width for responsivity purposes
 Chart.defaults.font.size = 12
 
-// tension/ circle size/ 
+// tension/ circle size/ tit
 let pageWidth = document.body.offsetWidth;
 if (pageWidth < 600){
      Chart.defaults.font.size = 14
@@ -217,10 +251,11 @@ const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
 const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
 
 // selects the canves where to imped the first chart
-const monthChart = document.getElementById('hoursChart').getContext('2d');
+const dayChart = document.getElementById('hoursChart').getContext('2d');
+const monthChart = document.getElementById('monthChart').getContext('2d');
 
 
-const chart1 = new Chart(monthChart, {
+const chart1 = new Chart(dayChart, {
      // specifies the type of the chart(whether a bar, pie, table or a line chart)
      type: 'line',
      
@@ -233,6 +268,56 @@ const chart1 = new Chart(monthChart, {
                borderColor: '#8C7161',
                color: '#0d0d0d',
                data: [222.64, 222.3, 219.2, 219.19, 220.47, 227.24, 227, 227, 218, NaN, NaN, 234, 222],
+
+               pointRadius: 3.4,
+
+               segment: {
+                    borderColor: ctx => skipped(ctx, '#8C716188') || down(ctx, '#8C7161'),
+                    borderDash: ctx => skipped(ctx, [6, 6]),
+                  },
+               spanGaps: true,
+
+               tension: 0,
+          }]
+     },
+     // here we place the some of the styling propereties
+     options: {
+          maintainAspectRatio: false,
+          scales: {
+               y: {
+                    suggestedMin: 218,
+                    suggestedMax: 234,
+               },
+               x: {
+                    beginAtZero: true,
+                    grid: {
+                         display: false
+                    }
+               }
+          },
+          layout: {
+
+          },
+          responsive: true,
+          plugins: {
+               suggestedMin: 10,
+               suggestedMax: 10
+          }
+     }
+})
+const chart2 = new Chart(monthChart, {
+     // specifies the type of the chart(whether a bar, pie, table or a line chart)
+     type: 'line',
+     
+     // here we place our data 
+     data: {
+          labels: days,
+          datasets: [{
+               label: textuals.en.thrdCard.title,
+               backgroundColor: '#8C7161aa',
+               borderColor: '#8C7161',
+               color: '#0d0d0d',
+               data: [222.64, 222.3, 219.2, 219.19, 220.47, 227.24, 227, 227, 218, NaN, 234, NaN, 222,227, 218, NaN, NaN, 234, 222,  222.3, 219.2, 219.19,227, 218, NaN, NaN, 234, 222, 220.47, 227.24, 227, 227, 218, NaN],
 
                pointRadius: 3.4,
 
