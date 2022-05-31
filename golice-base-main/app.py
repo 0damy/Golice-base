ffa1291e627353f02c1ef59b7a51670714763aaf
0damy <damy@Mohammeds-MacBook-Air.local>
2022-05-31 13:22:50 +0300

    reshaped the files in diffferent directories and flask is working now

--- golice-base-main/app.py
+++ golice-base-main/app.py
@@ -17,7 +17,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///pirceLog.db")
+db = SQL("sqlite:///priceLog.db")
 
 
 
@@ -27,21 +27,15 @@ def myApiCall():
     url = 'https://api.metals.live/v1/spot'
     response = requests.get(url)
     response.raise_for_status()
-    quote = response.json()[0]['gold']
-#     here i get the current time to use it when storing data in the db
-    current_time = datetime.datetime.now(pytz.timezone('Asia/Riyadh'))
-    current_time.month
+    quote = float(response.json()[0]["gold"])
+    #here i get the current time to use it when storing data in the db
+    current_time = datetime.now(pytz.timezone('Asia/Riyadh'))
     # here is where i store the results in the data base
     db.execute('INSERT INTO historical Values(null, ?, ?, ?, ?, ?)', quote, current_time.year, current_time.month, current_time.day, current_time.hour)
-
-    # simple test
-    print('saex')
     # call myApi() again in 6000 seconds/1hour
     threading.Timer(6000, myApiCall).start()
 
 myApiCall()
-
-print('heloszzzz')
 
 #    my only table in the data base has this schema:
 
@@ -53,3 +47,20 @@ print('heloszzzz')
 # day INTEGER      NOT NULL,
 # hour INTEGER     NOT NULL    
 # );
+
+# here is where i put all the redirections and the server things
+
+@app.route('/')
+def index():
+     # import from db and forward to html pages
+     month = db.execute('SELECT * FROM historical GROUP BY day ORDER BY id ASC LIMIT 30')
+     currento = datetime.now(pytz.timezone('Asia/Riyadh'))
+     day =  db.execute('SELECT * FROM historical WHERE day = ? ORDER BY id DESC', currento.day)
+     return render_template('index.html', month=month, day=day)
+
+# @app.route('/about')
+#      def about():
+
+# @app.route('/info')
+#      def info():
+     
